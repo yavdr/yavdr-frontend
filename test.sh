506f0d092920247ddd716f69054e9f4a82e04b7f
Marco Scholl <develop@marco-scholl.de>
2012-06-10 12:22:58 +0200

    remote test script

--- test.sh
+++
@@ -1,6 +0,0 @@
-
-  . /usr/share/yavdr/helpers/conffiles
-  remove_conffile yavdr-frontend-base /etc/init/frontend.conf
-  remove_conffile yavdr-frontend-base /etc/init/frontend-dummy.conf
-  remove_conffile yavdr-frontend-base /etc/init/frontend-softhddevice.conf
-
