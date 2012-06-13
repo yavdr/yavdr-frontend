4ca310eeeaa300a107ef2f83486406bc92b48243
Marco Scholl <develop@marco-scholl.de>
2012-06-13 23:19:48 +0200

    add logging

--- softhddevice/templates/etc/init/frontend-softhddevice.conf/47_pre_start_body.cs
+++ softhddevice/templates/etc/init/frontend-softhddevice.conf/47_pre_start_body.cs
@@ -1,2 +1,4 @@
-  /usr/bin/vdr-dbus-send /Remote remote.Enable ||: &> /dev/null
-  /usr/bin/vdr-dbus-send /Plugins/softhddevice plugin.SVDRPCommand string:'ATTA' string:"-d $DISPLAY" ||: &> /dev/null
+  logger -t frontend-softhddevice "activate remote"
+  vdr-dbus-send /Remote remote.Enable ||: &> /dev/null
+  logger -t frontend-softhddevice "activate softhddevice on $DISPLAY"
+  vdr-dbus-send /Plugins/softhddevice plugin.SVDRPCommand string:'ATTA' string:"-d $DISPLAY" ||: &> /dev/null
--- softhddevice/templates/etc/init/frontend-softhddevice.conf/87_post_stop_body.cs
+++ softhddevice/templates/etc/init/frontend-softhddevice.conf/87_post_stop_body.cs
@@ -1,2 +1,4 @@
+  logger -t frontend-softhddevice "deactivate remote"
   /usr/bin/vdr-dbus-send /Remote remote.Disable ||: &> /dev/null
+  logger -t frontend-softhddevice "deactivate softhddevice"
   /usr/bin/vdr-dbus-send /Plugins/softhddevice plugin.SVDRPCommand string:'DETA' ||: &> /dev/null
