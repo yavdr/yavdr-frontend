dee89b5cc49379bd3f7317f7175cb4df430573df
Lars Hanisch <dvb@flensrocker.de>
2013-05-12 11:24:12 +0200

    correct some vdr-dbus-send calls

--- softhddevice/templates/etc/init/frontend-softhddevice.conf/87_post_stop_body.cs
+++ softhddevice/templates/etc/init/frontend-softhddevice.conf/87_post_stop_body.cs
@@ -1,4 +1,4 @@
   logger -t frontend-softhddevice "deactivate remote"
   /usr/bin/vdr-dbus-send /Remote remote.Disable ||: &> /dev/null
   logger -t frontend-softhddevice "deactivate softhddevice"
-  /usr/bin/vdr-dbus-send /Plugins/softhddevice plugin.SVDRPCommand string:'DETA' ||: &> /dev/null
+  /usr/bin/vdr-dbus-send /Plugins/softhddevice plugin.SVDRPCommand string:'DETA' string:'' ||: &> /dev/null
